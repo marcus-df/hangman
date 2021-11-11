6fec93f84da5e59150d096251055dc1220227743
Marcus Falck <marcusfalck@fastmail.fm>
2021-11-11 13:19:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Variabler som kopplas till de olika elementen i html:en 
-const startField = document.getElementById("start-field"); // gör så att startfältet göms efter den avlyssnats. 
+const startField = document.getElementById("start-field");
 const startInput = document.getElementById("start-input");
 const startBtn = document.getElementById("start-btn");
 
 const gameField = document.getElementById("game-field");
-const gameLetters = document.getElementById("game-letters"); // kolumnerna med det gömda ordet
+const gameLetters = document.getElementById("game-letters"); // Kolumn som innehåller det gömda ordet
 const gameBtn = document.getElementById("game-btn");
 
-// Gubben uppdelad
+// SVGn's delar
 const ground = document.getElementById("ground");
 const scaffold = document.getElementById("scaffold");
 const head = document.getElementById("head");
@@ -26,6 +26,10 @@ const endTitle = document.getElementById("end-title");
 const restartBtn = document.getElementById("restart-btn");
 
 let theWord = [];
+let correctLetters = [];
+let failCount = 0;
+let intervalID;
+let bonusPoints = 0;
 
 // Anropar startBtn.addEventListener("click", generateWord);
 startInput.addEventListener("keypress", function (event) {
@@ -43,7 +47,7 @@ function generateWord() {
 
     resetInput();
     hideHangman();
-    // startTimer(90);
+    startTimer(90);
 
     for (let i = 0; i < theWord.length; i++) {
         let letter = theWord[i];
@@ -64,9 +68,6 @@ guessInput.addEventListener("keypress", function (event) {
 
 gameBtn.addEventListener("click", compareLetter);
 
-let correctLetters = [];
-let failCount = 0;
-
 // Funktion som jämför det inmatade värdet med Ordet (från startInput)
 function compareLetter() {
 
@@ -76,7 +77,7 @@ function compareLetter() {
 
     correctLetters.length = theWord.length;
 
-    //Rensar guessInput fältet efter varje gissad bokstav
+    // Rensar guessInput fältet efter varje gissad bokstav
     guessInput.value = "";
 
     if (result[0] != undefined) {
@@ -94,9 +95,8 @@ function compareLetter() {
         failCount++;
     };
 
-    // Switch case, som kollar på värdet av failCount och tar bort klasser efter failCounts värde.
+    // Switch case, kollar värdet av failCount och tar bort klasser baserat på failCounts värde.
     switch (failCount) {
-
         case 1:
             ground.classList.remove("hang-hidden");
             break;
@@ -129,6 +129,7 @@ function compareLetter() {
 
 // Detta händer när man klarat spelet
 function winCondition() {
+    clearInterval(intervalID);
     // Scrollar ned till botten
     window.scrollTo(0, document.body.scrollHeight);
     let remainingGuesses = 6 - failCount;
@@ -149,16 +150,15 @@ function winCondition() {
 }
 
 function loseCondition() {
+    clearInterval(intervalID);
     // Scrollar ned till botten
-
     window.scrollTo(0, document.body.scrollHeight);
     document.getElementById("end-title").innerHTML = "Oh no! You lost!";
-    endScore.innerHTML = "You have no attempts remaining. <br> The word was " + theWord.join("");
+    endScore.innerHTML = "You ran out of attempts or time. <br> The word was " + theWord.join("");
 }
 
 // Restart
 restartBtn.addEventListener('click', function () {
-
     document.getElementById("end-title").innerHTML = "You completed the word!";
 });
 
@@ -171,7 +171,6 @@ function hideHangman() {
     legs.classList.add("hang-hidden");
 }
 
-// Reset
 function resetInput() {
     // Nollställer och tar bort ordet från input-fältet.
     startInput.value = "";
@@ -186,18 +185,31 @@ function resetInput() {
     correctLetters = [];
 }
 
-// function startTimer(time) {
+// Timerfunktion, bestämmer tiden som spelaren har på sig.
+function startTimer(time) {
+    intervalID = setInterval(timer, 1000);
+    let remainTime = time;
 
-//     let second = setInterval(timer, 100);
-//     let remainTime = time;
+    function timer() {
+        remainTime--;
+        displayTime.innerText = remainTime + "s remaining!";
 
-//     function timer() {
-//         remainTime--;
-//         displayTime.innerText = remainTime + "s remaining!";
+        if (remainTime <= 0) {
+            loseCondition();
+            remainTime = 9999;
+        }
+    };
+};
 
-//         if (remainTime <= 0) {
-//             loseCondition();
-//             clearInterval(second);
-//         }
-//     };
-// };
+// Filterfunktion som känner av efter knapptryckningar som matchar alfabetet annars raderar den det sist inmatade värdet
+startInput.addEventListener("keyup", function () {
+    if (!startInput.value.match(/^[a-zA-z]+$/) && startInput.value != "") {
+        startInput.value = startInput.value.slice(0, -1);
+    }
+});
+
+guessInput.addEventListener("keyup", function () {
+    if (!guessInput.value.match(/^[a-zA-z]+$/) && guessInput.value != "") {
+        guessInput.value = guessInput.value.slice(0, -1);
+    }
+});
